39fc406bdd0f6912cd9d157d06c56db4e4ee40ce
chandrakant <chandra.kant@kellton.com>
2023-02-13 18:00:41 +0530

    Async Queue POC Added

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const async = require("async");
 const express = require("express");
 const app = express();
 const http = require("http");
@@ -13,22 +14,33 @@ const io = new Server(server, {
     methods: ["GET", "POST"],
   },
 });
+
 const users = {}
 
 io.on('connection', socket => {
+  const worker = function(task , callback){
+    console.log("Message  :=>  "+task.message);
+    callback();
+  }
   socket.on('new-user', name => {
     users[socket.id] = name
     socket.broadcast.emit('user-connected', name)
+    queue.push({message:"A new user is connected name :"+name})
   })
   socket.on('send-chat-message', message => {
     socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
+    queue.push({message:"Message from "+ users[socket.id]})
   })
   socket.on('disconnect', () => {
     socket.broadcast.emit('user-disconnected', users[socket.id])
+    queue.push({message: users[socket.id] + "is Disconnected !"});
     delete users[socket.id]
   })
+  const queue = async.queue(worker,1)
 });
-
+// queue.drain(()=>{
+//   console.log("All the tasks in queue are processed  !");
+// })
 server.listen(5000, () => {
   console.log("SERVER RUNNING");
 });
